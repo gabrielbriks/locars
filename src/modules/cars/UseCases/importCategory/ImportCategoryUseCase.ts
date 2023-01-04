a05501277e36832c24336ada0f2129ee9bc04747
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2023-01-04 10:25:35 -0300

    Implementando leitura de arquivo csv para salvar dados no banco
    Finalizando leitura arquivo csv de categorias para inserir os dados do
    arquivo no banco de dados.

--- src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
@@ -1,24 +1,52 @@
 
 import { parse as csvParse } from 'csv-parse';
 import fs from 'fs';
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
 export class ImportCategoryUseCase {
-  constructor(){}
+  constructor(private categoriesRepository: CategoriesRepository){}
 
-  execute(file: Express.Multer.File): void {
-    /**Conceito básico de stream
-     * Lê ou disponibiliza um conteúdo em partes de forma progressiva, com um video
-     */
-    const stream = fs.createReadStream(file.path);
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
 
-    const parseFile = csvParse();
+    return new Promise((resolve, reject) => {
+      /**Conceito básico de stream
+      * Lê ou disponibiliza um conteúdo em partes de forma progressiva, com um video
+      */
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = []
 
-    /** pipe()
-     * Pega o que esta sendo lido dentro do stream e nos disponibiliza para manipularmos
-     */
-    stream.pipe(parseFile);
+      const parseFile = csvParse();
 
-    parseFile.on("data", async (line)=> {
-      console.log(line);
-    })
+      /** pipe()
+      * Pega o que esta sendo lido dentro do stream e nos disponibiliza para manipularmos
+      */
+      stream.pipe(parseFile);
+
+      parseFile.on("data", async (line)=> {
+        const [name, description] = line;
+
+        categories.push({
+          name,
+          description
+        });
+      })
+      .on("end", () => {
+        resolve(categories)
+      })
+      .on("error", (err) => {
+        reject(err)
+      })
+    });
+
+  }
+ 
+   async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
   }
 }
