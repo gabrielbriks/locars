1bce76c018e4d175a5f72460f000a80dae6d2c85
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2023-01-04 09:40:32 -0300

    Implementação inicial da leitura de arquivo com "csvParse" e "stream"

--- src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,24 @@
+
+import { parse as csvParse } from 'csv-parse';
+import fs from 'fs';
 export class ImportCategoryUseCase {
   constructor(){}
 
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    /**Conceito básico de stream
+     * Lê ou disponibiliza um conteúdo em partes de forma progressiva, com um video
+     */
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    /** pipe()
+     * Pega o que esta sendo lido dentro do stream e nos disponibiliza para manipularmos
+     */
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line)=> {
+      console.log(line);
+    })
   }
 }
